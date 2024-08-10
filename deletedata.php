bacfbfc9104bccbf1b5924e18be8193bec04785f
shweta <shwetakidile@gmail.com>
2024-08-10 17:20:00 +0530

    deletion file is added

---
+++ deletedata.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "conn.php";
+    $id = $_GET["id"];
+    $query = "DELETE FROM results WHERE id = '$id'";
+    if (mysqli_query($conn, $query)) {
+        header("location: index.php");
+    } else {
+         echo "Something went wrong. Please try again later.";
+    }
+?>
+     
